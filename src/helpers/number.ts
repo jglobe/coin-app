a70b265faf95ca25f2a264f0b5b33859fadbb0c1
jglobe <shrkss0@gmail.com>
2023-03-30 18:43:32 +0300

    Fix format numbers, add format in coin page

--- src/helpers/number.ts
+++ src/helpers/number.ts
@@ -3,19 +3,19 @@ const trillion = 1000000000000;
 const billion = 1000000000;
 const million = 1000000;
 
-export function formatUsd(value:string|number) {
+export function formatCurrency(value:string|number, currency:string) {
   if(value === null) return '-';
-  if(+value > trillion) return (+value/trillion).toFixed(2)+'t';
-  if(+value > billion) return (+value/billion).toFixed(2)+'b';
-  if (+value > million) return (+value/million).toFixed(2)+'m';
-  if (+value < 1 && +value > -1) return `${(+value).toPrecision(2)}`;
+  if(+value > trillion) return currency+(+value/trillion).toFixed(2)+'t';
+  if(+value > billion) return currency+(+value/billion).toFixed(2)+'b';
+  if (+value > million) return currency+(+value/million).toFixed(2)+'m';
+  if (+value < 1 && +value > -1) return currency+`${(+value).toPrecision(2)}`;
 
-  return `${(+value).toFixed(2)}`;
+  return `${currency}${(+value).toFixed(2)}`;
 }
 
 export function formatPercent(value:string|number) {
-  if(value === null) return '-';
-  if (+value < 1 && +value > -1) return `${(+value).toPrecision(2)}`;
+  if(value === null) return '0';
+  if (+value < 1 && +value > -1) return `${(+value).toPrecision(2)}%`;
 
-  return `${(+value).toFixed(2)}`;
+  return `${(+value).toFixed(2)}%`;
 }
--- src/pages/coin/coin-page.tsx
+++ src/pages/coin/coin-page.tsx
@@ -1,71 +1,106 @@
+import { Fragment, useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+
 import { Button } from '@/components/button';
 import { Input } from '@/components/input';
 
+import { formatPercent, formatCurrency } from '@/helpers/number';
+import * as coincapServices from '@/services/coincap.service';
+
 import styles from './coin-page.module.scss';
+import classNames from 'classnames';
 
 export function CoinPage() {
+  const emptyCoinData = {} as coincapServices.CoinPropsType;
+  const [coin, setCoin] = useState(emptyCoinData);
+
+  let { id } = useParams();
+
+  useEffect(() => {
+    async function getCoinById(id:string) {
+      try {
+        const currentCoin = await coincapServices.getCoin(id);
+
+        setCoin(currentCoin);
+      } catch(error) {
+        console.error(error)
+      }
+    }
+
+    if(id) getCoinById(id);
+
+  }, [id])
+
   return(
     <div className={styles.page}>
-      <h2 className={styles.page__title}>Bitcoin</h2>
-      <div className={styles.page__diagram}>
-        daigram
-      </div>
-      <div className={styles.coin}>
-        <div className={styles.coin__prop}>
-          Name:
-          <span className={styles.coin__value}>
-            Bitcoin
-          </span>
-        </div>
-        <div className={styles.coin__prop}>
-          Symbol:
-          <span className={styles.coin__value}>
-            BTC
-          </span>
-        </div>
-        <div className={styles.coin__prop}>
-          Market Cap:
-          <span className={styles.coin__value}>
-            2131
-          </span>
-        </div>
-        <div className={styles.coin__prop}>
-          VWAP (24Hr):
-          <span className={styles.coin__value}>
-            32135
-          </span>
-        </div>
-        <div className={styles.coin__prop}>
-          Supply:
-          <span className={styles.coin__value}>
-            321351
-          </span>
-        </div>
-        <div className={styles.coin__prop}>
-          Volume (24Hr):
-          <span className={styles.coin__value}>
-            3543513
-          </span>
-        </div>
-        <div className={styles.coin__prop}>
-          Change (24Hr):
-          <span className={styles.coin__value}>
-            351351351
-          </span>
-        </div>
+      {coin?.data && (
+        <Fragment>
+          <h2 className={styles.page__title}>{coin.data.name}</h2>
+          <div className={styles.page__diagram}>
+            daigram
+          </div>
+          <div className={styles.coin}>
+            <div className={styles.coin__prop}>
+              Name:
+              <span className={styles.coin__value}>
+                {coin.data.name}
+              </span>
+            </div>
+            <div className={styles.coin__prop}>
+              Symbol:
+              <span className={styles.coin__value}>
+                {coin.data.symbol}
+              </span>
+            </div>
+            <div className={styles.coin__prop}>
+              Market Cap:
+              <span className={styles.coin__value}>
+                {formatCurrency(coin.data.marketCapUsd, '$')}
+              </span>
+            </div>
+            <div className={styles.coin__prop}>
+              VWAP (24Hr):
+              <span className={styles.coin__value}>
+                {formatCurrency(coin.data.vwap24Hr, '$')}
+              </span>
+            </div>
+            <div className={styles.coin__prop}>
+              Supply:
+              <span className={styles.coin__value}>
+                {formatCurrency(coin.data.supply, '')}
+              </span>
+            </div>
+            <div className={styles.coin__prop}>
+              Volume (24Hr):
+              <span className={styles.coin__value}>
+                {formatCurrency(coin.data.volumeUsd24Hr, '$')}
+              </span>
+            </div>
+            <div className={styles.coin__prop}>
+              Change (24Hr):
+              <span className={classNames({
+                [styles.coin__value]: true,
+                [styles.coin__value_positive]: +coin.data.changePercent24Hr > 0,
+                [styles.coin__value_negative]: +coin.data.changePercent24Hr < 0
+              })}>
+                {formatPercent(coin.data.changePercent24Hr)}
+              </span>
+            </div>
 
-        <form className={styles.form}>
-          <Input
-            type="number"
-            placeholder='type...'
-          />
-          <Button
-            type='submit'
-          >
-            Buy
-          </Button>
-        </form>
-      </div>
+            <form className={styles.form}>
+              <Input
+                type="number"
+                placeholder='type...'
+              />
+              <Button
+                type='submit'
+              >
+                Buy
+              </Button>
+            </form>
+          </div>
+        </Fragment>
+      )}
     </div>
   )
 }

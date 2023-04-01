5ee60018aca6dfc27126417037dde44e15c8f65c
jglobe <shrkss0@gmail.com>
2023-04-02 00:59:28 +0300

    Add back button & styling coin page

--- src/pages/coin/coin-page.tsx
+++ src/pages/coin/coin-page.tsx
@@ -1,5 +1,6 @@
 import { Fragment, useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router';
+import { useNavigate } from 'react-router-dom';
 import classNames from 'classnames';
 
 import { Button } from '@/components/button';
@@ -44,68 +45,23 @@ export function CoinPage() {
     coin.data.id && count && context.addTransaction({ current: coin.data, count: +count });
     form.reset();
   }
+  let navigate = useNavigate();
 
   return(
-    <div className={styles.page}>
-      {coin?.data && (
-        <Fragment>
-          <h2 className={styles.page__title}>{coin.data.name}</h2>
-          <div className={styles.page__diagram}>
-            <Diagram id={coin.data.id} />
-          </div>
-          <div className={styles.coin}>
-            <div className={styles.coin__prop}>
-              Name:
-              <span className={styles.coin__value}>
-                {coin.data.name}
-              </span>
+    <>
+      <div className={styles.page}>
+        {coin?.data && (
+          <Fragment>
+            <h2 className={styles.page__title}>{coin.data.name}</h2>
+            <div className={styles.page__diagram}>
+              <Diagram id={coin.data.id} />
             </div>
-            <div className={styles.coin__prop}>
-              Symbol:
-              <span className={styles.coin__value}>
-                {coin.data.symbol}
-              </span>
-            </div>
-            <div className={styles.coin__prop}>
-              Market Cap:
-              <span className={styles.coin__value}>
-                {formatCurrency(coin.data.marketCapUsd, '$')}
-              </span>
-            </div>
-            <div className={styles.coin__prop}>
-              VWAP (24Hr):
-              <span className={styles.coin__value}>
-                {formatCurrency(coin.data.vwap24Hr, '$')}
-              </span>
-            </div>
-            <div className={styles.coin__prop}>
-              Supply:
-              <span className={styles.coin__value}>
-                {formatCurrency(coin.data.supply, '')}
-              </span>
-            </div>
-            <div className={styles.coin__prop}>
-              Volume (24Hr):
-              <span className={styles.coin__value}>
-                {formatCurrency(coin.data.volumeUsd24Hr, '$')}
-              </span>
-            </div>
-            <div className={styles.coin__prop}>
-              Change (24Hr):
-              <span className={classNames({
-                [styles.coin__value]: true,
-                [styles.coin__value_positive]: +coin.data.changePercent24Hr > 0,
-                [styles.coin__value_negative]: +coin.data.changePercent24Hr < 0
-              })}>
-                {formatPercent(coin.data.changePercent24Hr)}
-              </span>
-            </div>
-
             <form
               onSubmit={buyCoin}
               className={styles.form}
             >
               <Input
+                className={styles.form__input}
                 type="number"
                 placeholder='0.000001'
                 name='count'
@@ -119,9 +75,63 @@ export function CoinPage() {
                 Buy
               </Button>
             </form>
-          </div>
-        </Fragment>
-      )}
-    </div>
+            <div className={styles.coin}>
+              <div className={styles.coin__prop}>
+                Name:
+                <span className={styles.coin__value}>
+                  {coin.data.name}
+                </span>
+              </div>
+              <div className={styles.coin__prop}>
+                Symbol:
+                <span className={styles.coin__value}>
+                  {coin.data.symbol}
+                </span>
+              </div>
+              <div className={styles.coin__prop}>
+                Market Cap:
+                <span className={styles.coin__value}>
+                  {formatCurrency(coin.data.marketCapUsd, '$')}
+                </span>
+              </div>
+              <div className={styles.coin__prop}>
+                VWAP (24Hr):
+                <span className={styles.coin__value}>
+                  {formatCurrency(coin.data.vwap24Hr, '$')}
+                </span>
+              </div>
+              <div className={styles.coin__prop}>
+                Supply:
+                <span className={styles.coin__value}>
+                  {formatCurrency(coin.data.supply, '')}
+                </span>
+              </div>
+              <div className={styles.coin__prop}>
+                Volume (24Hr):
+                <span className={styles.coin__value}>
+                  {formatCurrency(coin.data.volumeUsd24Hr, '$')}
+                </span>
+              </div>
+              <div className={styles.coin__prop}>
+                Change (24Hr):
+                <span className={classNames({
+                  [styles.coin__value]: true,
+                  [styles.coin__value_positive]: +coin.data.changePercent24Hr > 0,
+                  [styles.coin__value_negative]: +coin.data.changePercent24Hr < 0
+                })}>
+                  {formatPercent(coin.data.changePercent24Hr)}
+                </span>
+              </div>
+            </div>
+          </Fragment>
+        )}
+      </div>
+      <Button
+        onClick={() => navigate(-1)}
+        className={styles.backButton}
+      >
+          Back
+      </ Button>
+    </>
   )
 }

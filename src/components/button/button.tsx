fd77ecbc1d8f986b41051d6ca097e6c5816cbba9
jglobe <shrkss0@gmail.com>
2023-04-03 12:26:43 +0300

    Fix unuseful destructurization

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -4,7 +4,7 @@ import styles from './button.module.scss';
 interface ButtonPropsType extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 }
 
-export function Button({...props}:ButtonPropsType) {
+export function Button(props:ButtonPropsType) {
   return(
     <button
       {...props}

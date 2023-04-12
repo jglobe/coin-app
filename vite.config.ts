53af7ecb6c57d3e76e792d21577db2ee5af6ff26
jglobe <shrkss0@gmail.com>
2023-04-12 18:32:51 +0300

    Use jest/vitest to testing number functions

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: {
+    environment: 'jsdom',
+  },
   plugins: [react()],
   resolve: {
     alias: {
